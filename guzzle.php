4144f67651093a55662816c614d1f796a13ebcfb
Artur <artur.adimax@gmail.com>
2015-06-14 15:50:05 +0200

    now with holt.by

--- guzzle.php
+++ guzzle.php
@@ -9,20 +9,20 @@ use Symfony\Component\DomCrawler\Crawler;
  use Guzzle\Http\Message\Response;
 
   /** @var $client Client */
- $client = new Client("http://www.tut.by/");
+ $client = new Client("http://holt.by/texnika-dlya-kuxni/myasorubki.html");
 
  $request = $client->get();
  $response = $request->send();
  $body = $response->getBody(true);
  
-// echo $body;
+ //echo $body;
  
  $crawler = new Crawler($body);
 
 
-//var_dump($crawler->filter('#pageLogo')->attr('src'));
+//var_dump($crawler->filter('.browseProductImageContainer:first-child img')->first()->attr('src'));
 
-$linkToLogo = $crawler->filter('#pageLogo')->attr('src');
+$linkToLogo = $crawler->filter('.browseProductImageContainer:first-child img')->first()->attr('src');
 
 
 echo "<img src='$linkToLogo' />";
